db7238e7f5b89c53e909a5cbff026c52b4d97b29
kaedenwile <kaeden.wile@outlook.com>
2018-09-05 09:45:43 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='drizzle',
+      version='0.1',
+      description='A painless deployment system for modern code',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
